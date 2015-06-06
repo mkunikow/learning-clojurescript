caf65eb352a3ac43abec2d25cc1ae1ea8133bbf9
mkunikowski <michal.kunikowski@gmail.com>
2015-06-06 21:28:22 +0200

    averages

--- chapter3/finish/averages/project.clj
+++ chapter3/finish/averages/project.clj
@@ -2,7 +2,8 @@
   :description "Calculate various averages"
   :url "http://example.com/averages"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3169"]]
+                 [org.clojure/clojurescript "0.0-3169"]
+                 [domina "1.0.3"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
   :node-dependencies [[source-map-support "0.2.8"]]
   :plugins [[lein-npm "0.4.0"]]
--- chapter3/start/averages/src/averages/core.cljs
+++ chapter3/start/averages/src/averages/core.cljs
@@ -1,9 +1,44 @@
-(ns averages.core
-  (:require [clojure.browser.repl :as repl]))
+(ns ^:figwheel-always averages.core
+  (:require
+            [domina :as dom]
+            [domina.events :as ev]))
 
-(defonce conn
-  (repl/connect "http://localhost:9000/repl"))
+
+;;(ns averages.core
+;;  (:require [clojure.browser.repl :as repl]
+;;            [domina :as dom]
+;;            [domina.events :as ev]))
+
+;;(defonce conn
+;;  (repl/connect "http://localhost:9000/repl"))
+
+
+
 
 (enable-console-print!)
 
+(defn on-js-reload []
+  ;; optionally touch your app-state to force rerendering depending on
+  ;; your application
+  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+)
+
 (println "Hello world!")
+
+(defn arithmetic [a b] / (+ a b) 2.0)
+
+(defn geometric [a b] .sqrt js/Match (* a b))
+
+(defn harmonic [a b]
+  (/ 2 (+ (/ 1 a) (/ 1 b))))
+
+(defn float-value [id]
+  (.parseFloat js/window (dom/value (dom/by-id id))))
+
+(defn calculate-means [evt]
+  (dom/set-text! (dom/by-id "arithmetic") (arithmetic (float-value "A") (float-value "B")))
+  (dom/set-text! (dom/by-id "geometric") (geometric (float-value "A") (float-value "B")))
+  (dom/set-text! (dom/by-id "harmonic") (harmonic (float-value "A") (float-value "B"))))
+
+
+(ev/listen! (dom/by-id "calculate") "click" calculate-means)
